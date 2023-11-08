ba154a18f8b1814de44bf6644855ff167d30cef7
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-08 22:47:55 +0300

    The flutter odyssey

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,35 +1,53 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   final TextEditingController textController = TextEditingController();
   final TextEditingController textController2 = TextEditingController();
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-      appBar:AppBar(
-        title: Text("sign in"),
-
-      ),
-      body:Center(
-        child: Column(children: [
-          Text("user name"),
-          TextField(
-             controller: textController, 
-             decoration: InputDecoration(labelText: "password"),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("sign in"),
             ),
-          Text("password"),
-          TextField(
-             controller: textController2, 
-             decoration: InputDecoration(labelText: "password"),
-            ),
-          
-          
-        ],
-        ),
-      )
-      )
-    );
-
+            body:SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+        
+        
+                     Padding(
+                       padding: const EdgeInsets.all(35.0),
+                       child: TextField(
+                        //  controller: textController2,
+                        decoration: InputDecoration(
+                          labelText: "user name ",
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                                         ),
+                     ),
+                   
+                   
+                   
+              
+                    Padding(
+                      padding: const EdgeInsets.all(35.0),
+                      child: TextField(
+                        //  controller: textController2,
+                        decoration: InputDecoration(
+                          labelText: "password",
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+            )
+            );
   }
-
 }
