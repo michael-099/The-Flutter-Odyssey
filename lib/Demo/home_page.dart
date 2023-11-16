9ab16b12fd082dc3b6e7f1263f3f39ee103aedaf
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-16 11:59:57 +0300

    demo

--- lib/Demo/home_page.dart
+++ lib/Demo/home_page.dart
@@ -3,14 +3,16 @@ import 'package:namer_app/Demo/page2.dart';
 
 class Home_page extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Container(
-        child: ElevatedButton(
-            onPressed: () => {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (BuildContext context) {
-                    return page2();
-                  }))
-                },
-            child: Text("click")));
+    return Center(
+      child: Container(
+          child: ElevatedButton(
+              onPressed: () => {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (BuildContext context) {
+                      return page2();
+                    }))
+                  },
+              child: Text("click"))),
+    );
   }
 }
--- lib/Demo/page2.dart
+++ lib/Demo/page2.dart
@@ -5,6 +5,8 @@ class page2 extends StatefulWidget {
 }
 
 class Page2State extends State<page2> {
+  bool isSwitch = false;
+  bool? isCheckd = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,54 +19,73 @@ class Page2State extends State<page2> {
               },
               icon: Icon(Icons.arrow_back_ios_new_rounded)),
         ),
-        body: Column(
-          children: [
-            Image.asset('black.jpg'),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              padding: EdgeInsets.all(2.0),
-              height: 30,
-              width: double.infinity,
-              color: Colors.black,
-              child: Center(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('black.jpg'),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(2.0),
+                height: 30,
+                width: double.infinity,
+                color: Colors.green,
+                child: Center(
+                  child: Text(
+                    "hello",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: isSwitch ? Colors.blue : Colors.green,
+                ),
+                onPressed: () => {},
                 child: Text(
-                  "hello",
-                  style: TextStyle(color: Colors.white),
+                  "blue me  ",
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () => {},
-              child: Text(
-                "click me  ",
-              ),
-            ),
-            OutlinedButton(
-              onPressed: () => {},
-              child: Text(
-                "click me  ",
-              ),
-            ),
-            TextButton(
-              onPressed: () => {},
-              child: Text(
-                "click me  ",
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(
-                  Icons.local_fire_department,
-                  color:Colors.green,
+              OutlinedButton(
+                onPressed: () => {},
+                child: Text(
+                  "click me  ",
                 ),
-                 Icon(
-                  Icons.local_fire_department,
-                  color:Colors.green,
-                )
-              ],
-            )
-          ],
+              ),
+              TextButton(
+                onPressed: () => {},
+                child: Text(
+                  "click me  ",
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.green,
+                  ),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Switch(
+                  value: isSwitch,
+                  onChanged: (bool newBool) => {
+                        setState(() {
+                          isSwitch = newBool;
+                        })
+                      }),
+              Checkbox(
+                  value: isCheckd,
+                  onChanged: (bool? newBool) {
+                    setState(() {
+                      isCheckd = newBool;
+                    });
+                  })
+            ],
+          ),
         ));
   }
 }
