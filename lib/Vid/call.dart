d85a02f78d649e769565ca8810924bc9c33f947d
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-11-28 11:23:31 +0300

    vcall

--- lib/Vid/call.dart
+++ lib/Vid/call.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import "package:permission_handler/permission_handler.dart";
-import "package:agora_rtc_engine/rtc_local_view.dart" as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
 import "package:agora_rtc_engine/rtc_remote_view.dart" as RtcRemoteView;
 
 const AppID = "505a9ec196214b0db10c12bd06a7c237";
@@ -14,13 +14,23 @@ class VideoChat extends StatefulWidget {
 
 class VideoChatState extends State<VideoChat> {
   int _remoteUid = 0;
+  RtcEngine? _engine;
   @override
   void initState() {
     super.initState();
     initForAgora();
   }
 
-  Future<void> initForAgora() async {}
+  Future<void> initForAgora() async {
+    await [Permission.microphone, Permission.camera].request();
+    _engine = await RtcEngine.createWithConfig(RtcEngineConfig(AppID));
+    await _engine.enableVideo();
+    _engine.setEventHandler(RtcEngineEventHandler(
+        onJoinChannelSuccess: (String channel, int uid, int elapsed) {},
+        onUserJoined: (int uid, int elapsed) {},
+        userOffLine: (int uid, UserOffLineReason reason) {}));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("v chat")),
