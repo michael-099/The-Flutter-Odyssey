a81b55a476bdc82870c86213bccc13553f3aa64a
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-07 22:54:44 +0300

    setup

---
+++ lib/Final/button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  // final double radius;
+  final String label;
+  final double width;
+  final double height;
+  const Button(
+      {required this.label, required this.width, required this.height ,});
+
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+      child: Expanded(
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0) ,color: Colors.black,),
+          child: Center(
+            child: Text(
+              label,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
