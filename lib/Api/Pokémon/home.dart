390b27f0d4e3ffcc521c2bdc6064590b89b52a8a
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-07 22:40:26 +0300

    pokey

--- lib/Api/Pokémon/home.dart
+++ lib/Api/Pokémon/home.dart
@@ -1,10 +1,16 @@
 import "package:flutter/material.dart";
+import "package:http/http.dart" as http;
+import "dart:convert";
 
 class Home extends StatefulWidget {
   State createState() => HomeState();
 }
 
 class HomeState extends State<Home> {
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon/ditto"));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold();
   }
