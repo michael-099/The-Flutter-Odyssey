00069813685f07c5396c9e9f75caae5482233ab8
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-08 00:58:15 +0300

    setup

--- lib/Final/discription.dart
+++ lib/Final/discription.dart
@@ -2,13 +2,18 @@ import "package:flutter/material.dart";
 
 class Discription extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-      children: [
-        Text("Hello,"),
-        Text("login now."),
-        Text("welcome back please fillin the form to sign in and continue ")
-      ],
-    ));
+    return Align(alignment: Alignment.topLeft,
+      child: Container(
+        width:200,
+        height:100,
+          child: Column(
+        children: [ 
+          Align(alignment: Alignment.centerLeft,child:Text("Hello,"),),
+          Align(alignment: Alignment.centerLeft,child:Text("login now."),),
+          Align(alignment: Alignment.centerLeft,child:Text("welcome back please fillin the form to sign in and continue "),),
+        
+        ],
+      )),
+    );
   }
 }
