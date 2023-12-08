1f5fcf35a54c97ba7dc8aa2580d0a1a1e6b33ddf
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-08 13:57:07 +0300

    description box

--- lib/Final/discription.dart
+++ lib/Final/discription.dart
@@ -6,7 +6,7 @@ class Discription extends StatelessWidget {
       alignment: Alignment.topLeft,
       child: Container(
           width: 200,
-          height: 100,
+          height: 150,
           child: Column(
             children: [
               Align(
@@ -14,7 +14,7 @@ class Discription extends StatelessWidget {
                 child: Text("Hello,",
                     style: TextStyle(
                       color: Color.fromARGB(255, 0, 136, 20),
-                      fontSize: 30,
+                      fontSize: 40,
                       fontWeight: FontWeight.bold,
                       letterSpacing: 1,
                     )),
@@ -24,13 +24,20 @@ class Discription extends StatelessWidget {
                   child: Text("login now.",
                       style: TextStyle(
                           color: Colors.blue[90],
-                          fontSize: 30,
+                          fontSize: 40,
                           fontWeight: FontWeight.bold,
                           letterSpacing: 1))),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                    "welcome back please fillin the form to sign in and continue "),
+                  "welcome back please fillin the form to sign in and continue ",
+                  style: TextStyle(
+                    color: Colors.blue[90],
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                    
+                  ),
+                ),
               ),
             ],
           )),
