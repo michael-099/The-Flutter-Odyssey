8ed9ec94b8bd5a0b66c59fba072431324800185e
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-08 13:51:52 +0300

    description box

--- lib/Final/discription.dart
+++ lib/Final/discription.dart
@@ -2,18 +2,38 @@ import "package:flutter/material.dart";
 
 class Discription extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Align(alignment: Alignment.topLeft,
+    return Align(
+      alignment: Alignment.topLeft,
       child: Container(
-        width:200,
-        height:100,
+          width: 200,
+          height: 100,
           child: Column(
-        children: [ 
-          Align(alignment: Alignment.centerLeft,child:Text("Hello,"),),
-          Align(alignment: Alignment.centerLeft,child:Text("login now."),),
-          Align(alignment: Alignment.centerLeft,child:Text("welcome back please fillin the form to sign in and continue "),),
-        
-        ],
-      )),
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Hello,",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 136, 20),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    )),
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("login now.",
+                      style: TextStyle(
+                          color: Colors.blue[90],
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1))),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    "welcome back please fillin the form to sign in and continue "),
+              ),
+            ],
+          )),
     );
   }
 }
